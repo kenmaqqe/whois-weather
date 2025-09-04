df96de70a8e6d1c2a34d4c52dd35de666915009a
Kenmaqqe <dimetriytalibal@gmail.com>
2025-09-04 18:33:36 +0300

    fix bug

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    remotePatterns: [new URL("https://randomuser.me/api/portraits/**")],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        pathname: "/api/portraits/**",
+      },
+    ],
   },
 };
 
